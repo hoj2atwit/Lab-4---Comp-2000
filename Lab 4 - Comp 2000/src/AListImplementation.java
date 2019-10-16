09624c6521be698800d0b576469cb3c4e265df10
leey2 <leey2@WIT45001492.wit.private>
2019-10-15 20:27:29 -0400

    Renamed ArrayImplementation to AListImplementation

--- Lab 4 - Comp 2000/src/AListImplementation.java
+++ Lab 4 - Comp 2000/src/AListImplementation.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.util.Scanner; 
 
-public class ArrayImplementation {
+public class AListImplementation {
 
 	public static void main(String[] args) {
 		try {
